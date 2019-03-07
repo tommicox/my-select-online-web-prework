3bb25064bfc847ff0411d9dc76ab953300972028
Tommi Cox <tommicox@gmail.com>
2019-03-07 14:38:05 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,6 +1,11 @@
-def my_select(collection)
+def my_select(array)
 i = 0
-collect = []
-while i < collection.length
-
+select = []
+while i < array.length
+if yield(array[i])
+  select << array[i]
+end
+i+=1
+end
+select
 end
